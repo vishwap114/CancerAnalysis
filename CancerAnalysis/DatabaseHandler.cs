14ff6673f1c70e68fa7f82db924bcba18fa54409
shaishav <shaishav.shah.ce@gmail.com>
2019-11-29 16:17:15 -0500

    Added database connection creation logic

---
+++ CancerAnalysis/DatabaseHandler.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using CancerAnalysis.Model;
+using Oracle.ManagedDataAccess.Client;
+
+namespace CancerAnalysis
+{
+    public class DatabaseHandler
+    {
+        private OracleConnection connection;
+
+        public DatabaseHandler()
+        {
+            OpenConnection();
+        }
+
+        private void OpenConnection()
+        {
+            if (IsConnectionAlive()) return;
+
+            connection = new OracleConnection
+            {
+                ConnectionString = "User ID=sh3; Password=Netu0611; Data Source=oracle.cise.ufl.edu/orcl;"
+            };
+            connection.Open();
+        }
+
+        private bool IsConnectionAlive()
+        {
+            return connection != null && connection.State == ConnectionState.Open;
+        }
+
+        private void CloseConnection()
+        {
+            if(connection != null && connection.State == ConnectionState.Open)
+            {
+                connection.Close();
+            }
+        }
+
+        public List<CancerCase> GetNewCancerCases()
+        {
+            if (!IsConnectionAlive()) OpenConnection();
+
+            var result = new List<CancerCase>();
+
+            string query = "SELECT SITE_NAME AS CancerType, SUM(PATIENT_ID) AS TotalCases " +
+                "FROM pselugar.CASE C, mvdoshi.SITE_MASTER S " +
+                "WHERE C.SITE = S.SITE_ID AND C.YEAR_OF_DIAGNOSIS = 2016 " +
+                "GROUP BY SITE_NAME ORDER BY TotalCases DESC";
+
+            using (OracleDataAdapter adapter = new OracleDataAdapter(query, connection))
+            {
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+                foreach (DataRow dr in dt.Rows)
+                {
+                    CancerCase cancerCase = new CancerCase
+                    {
+                        CancerType = dr["CancerType"].ToString(),
+                        TotalCases = long.Parse(dr["TotalCases"].ToString())
+                    };
+
+                    result.Add(cancerCase);
+                }
+            }
+
+            return result;
+        }
+
+        public void finish()
+        {
+            CloseConnection();
+        }
+    }
+}
---
+++ CancerAnalysis/Model/CancerCase.cs
@@ -0,0 +1,10 @@
+﻿namespace CancerAnalysis.Model
+{
+    public class CancerCase
+    {
+        public CancerCase() { }
+
+        public string CancerType { get; set; }
+        public long TotalCases { get; set; }
+    }
+}
